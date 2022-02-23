955a6d0d3901c2e7e69fab23dd2453687fb2ba56
Shirisha Reddy <92243486+ShirishaReddy92@users.noreply.github.com>
2022-02-23 21:40:46 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool githubwebhook !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
